1be7e04ddb477ccbfc2f8368f9cd7d03c06fb7d3
Kory <korymbecker@gmail.com>
2022-02-11 12:54:11 -0500

    added reqAuth middleware, removed some imports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import chat from "./routes/chat.route";
+import register from "./routes/register.route";
+import login from "./routes/login.route";
+import logout from "./routes/logout.route";
+import me from "./routes/me.route";
 import connectDB from "./db/connect";
 import config from "./config/config";
-import cors from "cors";
+import requireAuth from "./middleware/auth.middleware";
+import errorHandler from "./middleware/error.middleware";
+import reqAuth from "./middleware/reqAuth.middleware";
 
 const app = express();
 
 // middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(reqAuth);
 
 // routes
-app.use("/api/v1", chat);
+app.use("/api/v1", chat, register, login, logout, me);
+app.use("/api/v1/chat", requireAuth);
+
+// error handler
+app.use(errorHandler);
 
 const PORT = config.server.port || 3001;
 
